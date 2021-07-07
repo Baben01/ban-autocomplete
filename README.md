120a6d58667ca62446f6f15bfdf2d41bf59842db
Benjamin Normand <bnormand@bordeaux-metropole.fr>
2021-07-07 12:51:09 +0200

    Some adjustment and initialisation of README

---
+++ README.md
@@ -0,0 +1,23 @@
+BAN-Autocomplete
+=============
+
+Une librairie Javascript permettant de générer une autocompletion à partir de la [Base Adresse Nationale](https://www.data.gouv.fr/fr/datasets/base-adresse-nationale/).
+
+Gère l'autocompletion des adresses à l'aide de la librairie Javascript [autoComplete.js](https://tarekraafat.github.io/autoComplete.js/#/)
+
+CommonJS
+--------
+### Installation
+
+TODO
+
+### Utilisation
+
+TODO
+
+
+Liens utiles
+-----------------
+
+* [Base Adresse Nationale](https://www.data.gouv.fr/fr/datasets/base-adresse-nationale/)
+* [autoComplete.js](https://tarekraafat.github.io/autoComplete.js/#/)
--- index.js
+++ index.js
@@ -1,63 +1,84 @@
 
 import autoComplete from "@tarekraafat/autocomplete.js";
+function BANAutocomplete()
+{
+	"use strict";
+	var autoCompleteJS = {};
+	var banApiEndpoint = "https://api-adresse.data.gouv.fr/"
+	autoCompleteConfig = {
+		threshold: 3,
+		searchEngine: "strict",
+		data: {
+		  src: async (query) => {
+			try {
+			  const source = await fetch(`${banApiEndpoint}search/?type=housenumber&autocomplete=1&limit=50&q=${query}`);
+			  const data = await source.json();
+			  return data.features.map(el => el.properties);
+			} catch (error) {
+			  return error;
+			}
+		  },
+		  keys: ["label"],
+		},
+		placeHolder: placeholder,
+		resultsList: {
+		  element: (list, data) => {
+			const info = document.createElement("p");
+			if (data.results.length <= 0) {
+			  info.innerHTML = `Searching...`;
+			}
+			list.prepend(info);
+		  },
+		  noResults: true,
+		  maxResults: 15,
+		  tabSelect: true
+		},
+		resultItem: {
+		  element: (item, data) => {
+			item.style = "";
+			item.innerHTML = `
+				<span style="text-overflow: ellipsis; white-space: nowrap; overflow: hidden; width: 100%;">
+				  ${data.match}
+				</span>
+				<span style="display: flex; justify-content: flex-end; font-size: 13px; font-weight: 100; text-transform: uppercase; color: rgba(0,0,0,.7);  width: 100%;">
+				  ${data.value.context}
+				</span>`;
+		  },
+		  highlight: true
+		},
+		events: {
+		  input: {
+			input: () => {
+			  if (autoCompleteJS.input.value.length && autoCompleteJS.input.value.length > autoCompleteJS.threshold) {
+				autoCompleteJS.start(autoCompleteJS.input.value);
+			  } 
+			},
+			focus:  () => {
+			  if (autoCompleteJS.input.value.length && autoCompleteJS.input.value.length > autoCompleteJS.threshold) {
+				autoCompleteJS.start(autoCompleteJS.input.value);
+			  } 
+			}
+		  }
+		}
+	}
+	
+	this.init = function () {
+		autoCompleteJS = new autoComplete(config);
+	}
 
-export function initBANAutocomplete(selector, placeholder) {
+	this.setName = function(value) {
+		autoCompleteConfig.name = value;
+	}
+	
+	this.setSelector = function(value) {
+		autoCompleteConfig.selector = value;
+	}
+	
+	this.setPlaceholder = function(value) {
+		autoCompleteConfig.placeholder = value;
+	}
 
-  const autoCompleteJS = new autoComplete({
-    selector: selector,
-    threshold: 3,
-    searchEngine: "strict",
-    data: {
-      src: async (query) => {
-        try {
-          const source = await fetch(`https://api-adresse.data.gouv.fr/search/?q=${query}&type=housenumber&autocomplete=1&limit=50`);
-          const data = await source.json();
-          return data.features.map(el => el.properties);
-        } catch (error) {
-          return error;
-        }
-      },
-      keys: ["label"],
-    },
-    placeHolder: placeholder,
-    resultsList: {
-      element: (list, data) => {
-        const info = document.createElement("p");
-        if (data.results.length <= 0) {
-          info.innerHTML = `Searching...`;
-        }
-        list.prepend(info);
-      },
-      noResults: true,
-      maxResults: 15,
-      tabSelect: true
-    },
-    resultItem: {
-      element: (item, data) => {
-        item.style = "";
-        item.innerHTML = `
-			<span style="text-overflow: ellipsis; white-space: nowrap; overflow: hidden; width: 100%;">
-			  ${data.match}
-			</span>
-			<span style="display: flex; justify-content: flex-end; font-size: 13px; font-weight: 100; text-transform: uppercase; color: rgba(0,0,0,.7);  width: 100%;">
-			  ${data.value.context}
-			</span>`;
-      },
-      highlight: true
-    },
-    events: {
-      input: {
-        input: () => {
-          if (autoCompleteJS.input.value.length && autoCompleteJS.input.value.length > autoCompleteJS.threshold) {
-            autoCompleteJS.start(autoCompleteJS.input.value);
-          } 
-        },
-        focus:  () => {
-          if (autoCompleteJS.input.value.length && autoCompleteJS.input.value.length > autoCompleteJS.threshold) {
-            autoCompleteJS.start(autoCompleteJS.input.value);
-          } 
-        }
-      }
-    }
-  });
+	this.setUrl = function (url) {
+		banApiEndpoint = url;
+	}
 }
